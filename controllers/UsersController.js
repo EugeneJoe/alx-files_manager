aade3f2f26d69506cb0352cf505b94f9d09e7e00
EugeneJoe <jmuthui70@gmail.com>
2022-02-11 10:26:17 +0000

    Add Queue for sending email to users

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,7 +1,10 @@
 import sha1 from 'sha1';
 import { ObjectID } from 'mongodb';
+import Queue from 'bull';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
+
+const userQueue = new Queue('userQueue', 'redis://127.0.0.1:6379');
 
 class UsersController {
   static postNew(request, response) {
@@ -28,8 +31,10 @@ class UsersController {
             email,
             password: hashedPassword,
           },
-        ).then((result) => response.status(201).json({ id: result.insertedId, email }))
-          .catch((error) => console.log(error));
+        ).then((result) => {
+	  response.status(201).json({ id: result.insertedId, email });
+	  userQueue.add({ userId: result.insertedId });
+	}).catch((error) => console.log(error));
       }
     });
   }
